2b322177333310b4d5d4e1d1f904c613ba0a734f
anantoj <gg.ananto@gmail.com>
2023-05-05 09:14:17 +0000

    added small jets config

--- egs2/bookbot/tts1/run.sh
+++ egs2/bookbot/tts1/run.sh
@@ -15,10 +15,20 @@
     --n_fft 1024 \
     --n_shift 256 \
     --win_length null \
-    --train_config conf/train_jets.yaml \
+    --train_config conf/train_small_jets.yaml \
     --inference_model latest.pth \
     --train_set train_nodev \
     --valid_set train_dev \
     --test_sets "train_dev test" \
     --srctexts "data/train_nodev/text"\
     --g2p bookbot_g2p
+    
+# num_iter: none
+# batch_bins: 10 mil
+# 50_batch speed = ~9 mins
+# mem_usage = high (38)
+
+# num_iter: 1000
+# batch_bins: 2 mil
+# 50_batch speed = ~2.5 mins
+# mem_usage = low (12)
